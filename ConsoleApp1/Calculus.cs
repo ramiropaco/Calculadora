ef650b2dbc21ff0c05a181d50b999405b0ac1e49
Ramiro Paco Escobar <ramiro.paco@endetransmision.bo>
2020-12-16 22:16:16 -0400

    A cada tarea que hayas realizado, le asignas un mensaje

---
+++ ConsoleApp1/Calculus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculus
+    {
+        public static double CalcularImpuesto(int precio) {
+            double impuesto = .16;
+            double resultado = 0;
+
+            resultado = (precio * impuesto) + precio;
+
+            return resultado;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            int[] precios = { 100, 200, 300 };
+            string[] productos = { "vaso", "plato", "cuchara" };
+            double resultado = 0;
+
+            for (int i = 0; i < productos.Length; i++)
+            {
+
+                 resultado += Calculus.CalcularImpuesto(precios[i]);
+
+                Console.WriteLine("Precio: " + precios[i]);
+                Console.WriteLine("resultado: " + resultado);
+                
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+   
+    }
+}
